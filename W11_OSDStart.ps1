095a14a0dcc18bfd45f0d3dba2cdeff3e64ca0b8
Andreas Schilling <andreas.schilling@sigvaris.com>
2024-12-28 10:15:14 +0100

    sfda

--- W11_OSDStart.ps1
+++ W11_OSDStart.ps1
@@ -306,7 +306,7 @@ $OOBECMD = @'
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\W11_Autopilot.ps1
 
 # Below a PS session for debug and testing in system context, # when not needed 
-# start /wait powershell.exe -NoL -ExecutionPolicy Bypass
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass
 
 exit 
 '@
