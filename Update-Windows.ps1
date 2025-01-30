6ff691a9a2f295b90338970855de90c8ac2fe48b
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-01-30 18:27:25 +0100

    adfaf

--- Update-Windows.ps1
+++ Update-Windows.ps1
@@ -54,6 +54,6 @@ else {
 Install-Module -Name PSWindowsUpdate -Force -Scope AllUsers -AllowClobber
 Import-Module PSWindowsUpdate -Scope Global
 Get-WindowsUpdate
-Install-WindowsUpdate -AcceptAll -IgnoreReboot
+Install-WindowsUpdate -AcceptAll -AutoReboot
 
 Stop-Transcript | Out-Null
