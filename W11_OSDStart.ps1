be79feed63f419f96748108680f0750dc7793e35
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-02-03 17:54:19 +0100

    asdfadf

--- W11_OSDStart.ps1
+++ W11_OSDStart.ps1
@@ -40,7 +40,6 @@ if ($UI) {
 $OSDComputername = (Get-WmiObject -Namespace "root\UIVars" -Class "Local_Config").OSDComputername
 $OSDLocation = (Get-WmiObject -Namespace "root\UIVars" -Class "Local_Config").OSDLocation
 $OSDLanguage = (Get-WmiObject -Namespace "root\UIVars" -Class "Local_Config").OSDLanguage
-$OSDDisplayLanguage = (Get-WmiObject -Namespace "root\UIVars" -Class "Local_Config").OSDDisplayLanguage
 $OSDKeyboard = (Get-WmiObject -Namespace "root\UIVars" -Class "Local_Config").OSDKeyboard
 $OSDKeyboardLocale = (Get-WmiObject -Namespace "root\UIVars" -Class "Local_Config").OSDKeyboardLocale
 $OSDGeoID = (Get-WmiObject -Namespace "root\UIVars" -Class "Local_Config").OSDGeoID
@@ -51,7 +50,6 @@ Write-Host -ForegroundColor Green "Your Settings are:"
 Write-Host "  Computername: $OSDComputername"
 Write-Host "  Location: $OSDLocation"
 Write-Host "  OS Language: $OSDLanguage"
-Write-Host "  Display Language: $OSDDisplayLanguage"
 Write-Host "  Keyboard: $OSDKeyboard"
 Write-Host "  KeyboardLocale: $OSDKeyboardLocale"
 Write-Host "  GeoID: $OSDGeoID"
@@ -235,7 +233,6 @@ $UIjson = @"
 {
     "OSDComputername" : "$OSDComputername",
     "OSDLanguage" : "$OSDLanguage",
-    "OSDDisplayLanguage" : "$OSDDisplayLanguage",
     "OSDLocation" : "$OSDLocation",
     "OSDKeyboard" : "$OSDKeyboard",
     "OSDKeyboardLocale" : "$OSDKeyboardLocale",
