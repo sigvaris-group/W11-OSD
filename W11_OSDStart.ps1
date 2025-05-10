fe4e761fdd960815051b81c8055c60cf28b18468
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-05-10 15:35:02 -0400

    adsf

--- W11_OSDStart.ps1
+++ W11_OSDStart.ps1
@@ -379,9 +379,6 @@ Write-Host -ForegroundColor Green "Downloading and creating script for OOBE phas
 $OOBECMD = @'
 @echo off
 
-# Below a PS session for debug and testing in system context, # when not needed 
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass
-
 # Execute OOBE Tasks
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\Import-WiFiProfiles.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\Install-PreApps.ps1
@@ -389,6 +386,9 @@ start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scri
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\scripts\Set-Language.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\Update-Windows.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\AutopilotBranding.ps1
+
+# Below a PS session for debug and testing in system context, # when not needed 
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass
 
 exit 
 '@
