024faa7a5bfb5c2c2dc2ffd703a5def925c24aa1
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-05-13 16:13:28 -0400

    afds

--- Update-Windows.ps1
+++ Update-Windows.ps1
@@ -91,7 +91,7 @@ If ($OSDWindowsUpdate -eq "Yes") {
         #Write-Host -ForegroundColor Green "Get Windows Updates"
         #Get-WindowsUpdate
         Write-Host -ForegroundColor Green "Install Windows Updates"
-        Install-WindowsUpdate -AcceptAll  -IgnoreReboot 
+        Install-WindowsUpdate -AcceptAll -IgnoreReboot 
 
         # Uninstall blocking language Update
         # icrosoft Community notes that after installing KB5050009, 
@@ -101,7 +101,7 @@ If ($OSDWindowsUpdate -eq "Yes") {
         #in English or a different language. This is particularly noticeable 
         # if additional languages were previously installed
         Write-Host -ForegroundColor Green "Uninstall KB5050009"
-        Remove-WindowsUpdate -KBArticleID KB5050009 -NoRestart
+        Remove-WindowsUpdate -KBArticleID KB5050009 -IgnoreReboot
 }
 else {
         Write-Host -ForegroundColor Yellow "No Windows Updates installed"
