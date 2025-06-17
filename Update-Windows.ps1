4fc4f9c9928bd8c94b2ecff33c0646548efb319f
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-06-17 16:30:24 +0200

    sfdg

--- Update-Windows.ps1
+++ Update-Windows.ps1
@@ -99,8 +99,8 @@ try {
         # such as the Settings side panel or desktop icon labels, 
         # in English or a different language. This is particularly noticeable 
         # if additional languages were previously installed
-        #Write-Host -ForegroundColor Green "Uninstall KB5050009"
-        #Remove-WindowsUpdate -KBArticleID KB5050009 -IgnoreReboot
+        Write-Host -ForegroundColor Green "Uninstall KB5050009"
+        Remove-WindowsUpdate -KBArticleID KB5050009 -IgnoreReboot
     }
     else {
         Write-Host -ForegroundColor Yellow "No Windows Updates installed"
