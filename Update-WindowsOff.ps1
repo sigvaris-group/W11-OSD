7caf00c37f06898888a090d368b909df31df4a5a
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-06-18 11:46:33 +0200

    asfd

--- Update-WindowsOff.ps1
+++ Update-WindowsOff.ps1
@@ -90,7 +90,7 @@ try {
         Import-Module PSWindowsUpdate -Scope Global
 
         Write-Host -ForegroundColor Green "Install Windows Updates"
-        Install-WindowsUpdate -KBArticleID KB5063060 -ForceInstall -AcceptAll -IgnoreReboot
+        Install-WindowsUpdate -KBArticleID KB5063060 -AcceptAll -IgnoreReboot
 
         # Uninstall blocking language Update
         # Microsoft Community notes that after installing KB5050009, 
