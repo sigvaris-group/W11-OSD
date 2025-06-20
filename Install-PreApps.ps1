18e18b7073d6efc43fbbd5688ffa7e4ca413d4b9
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-06-20 14:01:42 +0200

    asdf

--- Install-PreApps.ps1
+++ Install-PreApps.ps1
@@ -52,10 +52,6 @@ $IPConfig = Get-NetIPConfiguration
 Write-host -ForegroundColor Green "IPConfig before install Forescout"
 Write-Output $IPConfig
 
-$TestDomain = Test-NetConnection sigvaris-group.com -TraceRoute -InformationLevel Detailed -Verbose -ErrorAction SilentlyContinue
-Write-host -ForegroundColor Green "Test Domain Connection"
-Write-Output $TestDomain
-
 try {
 
     Write-Host -ForegroundColor Green "Install Forescout Secure Connector"
@@ -69,10 +65,6 @@ try {
     $IPConfig = Get-NetIPConfiguration
     Write-host -ForegroundColor Green "IPConfig after install Forescout"
     Write-Output $IPConfig
-
-    $TestDomain = Test-NetConnection sigvaris-group.com -TraceRoute -InformationLevel Detailed -Verbose -ErrorAction SilentlyContinue
-    Write-host -ForegroundColor Green "Test Domain Connection"
-    Write-Output $TestDomain
 
     Stop-Transcript | Out-Null
 } 
