ce73807a0e120f7761e525aa9ea6ec0bf45e624d
Andreas Schilling <andreas.schilling@sigvaris.com>
2025-06-24 10:49:28 +0200

    asdf

--- Set-LanguageOff.ps1
+++ Set-LanguageOff.ps1
@@ -112,7 +112,7 @@ try {
     # Install an additional language pack including FODs. With CopyToSettings (optional), this will change language for non-Unicode program.  
     If ($OSDDisplayLanguage -ne 'en-US') {
         Write-Host "    Install OS Language: $($OSDLanguage)"
-        Install-Language -Language $OSDLanguage -CopyToSettings
+        Install-Language -Language $OSDLanguage
         Write-Host "    Add Language Features: $($OSDLanguagePack)"
         Add-WindowsCapability -Online -Name "$OSDLanguagePack"
     }
